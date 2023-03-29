5b02c6f4b7e455a0607454f21dbbe0c95c383e8b
j3rryl <jeremymunroe0@gmail.com>
2023-03-29 18:51:56 +0300

    Minor tweaks

--- lib/pages/temp_page.dart
+++ lib/pages/temp_page.dart
@@ -5,8 +5,64 @@ class Temp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Home',
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.black,
+            expandedHeight: 300,
+
+            
+            // title: const Text("Work Tour"),
+            // centerTitle: true,
+            floating:false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                color: Colors.black54,
+
+                // decoration: const BoxDecoration(
+                //   gradient: LinearGradient(
+                //     colors: [
+                //       Colors.black,
+                //       Color(0x00434343)
+                //     ],
+                //     begin: Alignment.topLeft,
+                //     end: Alignment.bottomRight
+                //   )
+                // ),
+              ),
+              
+              title: const Text("Work Tour"),
+              centerTitle: true,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ClipRRect(
+                borderRadius:BorderRadius.circular(20),
+                child: Container(
+                    height: 400,
+                    color:Colors.deepPurple
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ClipRRect(
+                borderRadius:BorderRadius.circular(20),
+                child: Container(
+                    height: 400,
+                    color:Colors.deepPurple
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
